353a9c0413a2d9c5f5c86ed0dde9fdf811bd7a2a
Linda Hellborg <linda.hellborg@gmail.com>
2016-02-08 13:47:17 +0100

    add name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,20 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+var formattedName = "Linda Hellborg";
+var formattedRole = "Web Deveolper";
+
+var name = HTMLheaderName.replace("%data%", formattedName);
+var role = HTMLheaderRole.replace("%data%", formattedRole);
+
+$("#header").prepend(role);
+$("#header").prepend(name);
+
+
+
+$("#main").append("Linda");
+
+var awesomeThoughts = "I am Linda and I am AWESOME!";
+console.log(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN")
+
+$("#main").append(funThoughts);
